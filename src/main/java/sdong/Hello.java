9b8e652976d3176c04bbeac267f99c14ecb1f83f
shendong <shendong70@163.com>
2017-03-29 22:05:00 +0800

    test for scala

---
+++ src/main/java/sdong/Hello.java
@@ -0,0 +1,10 @@
+package sdong;
+
+/**
+ * Created by shendong on 2017/3/29.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
